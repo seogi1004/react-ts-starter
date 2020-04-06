c14dc1315b4c4fe9aadc170d7da2e65f87d4c506
alvin <seogi777@gmail.com>
2020-04-06 21:14:01 +0900

    First commit!!!

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        publicPath: '/',
+        path: path.join(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './',
+        inline: false,
+        hot: false,
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [ '.js', '.jsx', '.json' ],
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use:  [ MiniCssExtractPlugin.loader, 'css-loader' ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+};
